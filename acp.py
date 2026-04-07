aa1d787af542be3f3b74fbd7f85cce545077532e
Vivienne126 <vivienne.singh@cns.ac.in>
2026-04-07 22:52:07 +0530

    New123

---
+++ acp.py
@@ -0,0 +1,17 @@
+def myfunction1(n):
+    if(n>0):
+        return
+    for i in range (0,n+1):
+        print("Codingal")
+    myfunction1(n/2)
+    myfunction1(n/3)
+
+myfunction1(12)
+
+def myfunction2(n):
+    if(n<=1):
+        return
+    print("Codingal")
+    myfunction2(n-1)
+
+myfunction2(8)
